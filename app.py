a27c2ebd79281708f085fad29693540483d92965
irantheme <irantheme.com@gmail.com>
2023-09-10 14:45:19 +0330

    working on process function to receive data

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main_page():
+    ''' This is the main page of the site '''
+    return "<p>Main page</p>"
+
+
+@app.route('/v1/process', methods=['POST'])
+def process():
+    """ This is a callback from curl requests. will get sender and message and will check if it is valid, then answers back.  """
+    data = request.form
+    sender = data['from']
+    message = data['message']
+    print(f'Received: {message} from {sender}')
+    ret = {'message': 'processed!'}
+    return jsonify(ret), 200
+
+
+def send_sms():
+    pass
+
+
+def check_serial():
+    pass
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
