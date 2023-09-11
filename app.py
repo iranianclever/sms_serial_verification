c1b357fa107520e622a0021a9d1aa61e47443fbc
irantheme <irantheme.com@gmail.com>
2023-09-11 17:42:36 +0330

    add sqlite and inserted rows

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pandas import read_excel
+import sqlite3
 import requests
 import config
 
@@ -32,17 +33,69 @@ def send_sms(receptor, message):
     print(f'message *{message}* send to receptor: {receptor}. status code is {response.status_code}')
 
 def import_database_from_excel(filepath):
-    """ Gets an excel file name and imports lookup data (data and failures) from it. """
-    # df contains lookup data in the form of
-    # Row - Reference Number - Description - Start Serial - End Serial - Date
-    # df contains lookup data in the form of
-    df = read_excel(filepath, 0)
-    for index, (line, ref, desc, start_serial, end_serial, data) in df.iterrows():
-        print(line, ref, desc, start_serial, end_serial, data)
+    """ Gets an excel file name and imports lookup data (data and failures) from it.
+    the first sheet contains serial data like:
+     Row - Reference Number - Description - Start Serial - End Serial - Date
+    and the 2nd (1) contains a column of invalid serials.
 
+    This data will be written into the sqlite database located at config.DATA_FILE_PATH
+    in two tables. 'serials' and 'invalids'
+
+    return two integers: (number of serial rows, number of invalid rows)
+    """
+    # df contains lookup data in the form of
+    
+    # TODO: make sure that the data is imported correctly, we nned to backup the old one
+    # TODO: do some normalization
+
+    # Our sqlite database will contain two tables: serials and invalids
+    conn = sqlite3.connect(config.DATABASE_FILE_PATH)
+    cur = conn.cursor()
+
+    # Remove the serials table if exists, then create the new one
+    cur.execute('DROP TABLE IF EXISTS serials')
+    cur.execute("""CREATE TABLE IF NOT EXISTS serials (
+        id INTEGER PRIMARY KEY,
+        ref TEXT,
+        desc TEXT,
+        start_serial TEXT,
+        end_serial TEXT,
+        date DATE
+    );""")
+    conn.commit()
+
+    df = read_excel(filepath, 0)
+    serial_counter = 0
+    for index, (line, ref, desc, start_serial, end_serial, date) in df.iterrows():
+        query = f'INSERT INTO serials VALUES ("{line}", "{ref}", "{desc}", "{start_serial}", "{end_serial}", "{date}");'
+        cur.execute(query)
+        # TODO: do some more error handling
+        if serial_counter % 10:
+            conn.commit()
+        serial_counter += 1
+    conn.commit()
+
+    # Remove the invalid table if exists, then create the new one
+    cur.execute('DROP TABLE IF EXISTS invalids')
+    cur.execute("""CREATE TABLE IF NOT EXISTS invalids(
+                invalid_serial TEXT PRIMARY KEY
+    );""")
+    conn.commit()
+    invalid_counter = 0
     df = read_excel(filepath, 1)
-    for index, failed_serial in df.iterrows():
-        print(failed_serial)
+    for index, failed_serial_row in df.iterrows():
+        failed_serial = failed_serial_row.values[0]
+        query = f'INSERT INTO invalids VALUES ("{failed_serial}")'
+        cur.execute(query)
+        # TODO: do some more error handling
+        if invalid_counter % 10:
+            conn.commit()
+        invalid_counter += 1
+    conn.commit()
+
+    conn.close()
+
+    return (serial_counter, invalid_counter)
 
 
 def check_serial():
@@ -51,4 +104,5 @@ def check_serial():
 
 if __name__ == '__main__':
     # app.run('0.0.0.0', 5000, debug=True)
-    import_database_from_excel('./data.xlsx')
+    a, b = import_database_from_excel('./data.xlsx')
+    print(f'inserted {a} rows and {b} invalids')
